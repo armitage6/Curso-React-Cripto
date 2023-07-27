c81a579cb7ff0941fc4f61b94e61218877ba99ed
armitage6 <pablutus@gmail.com>
2023-07-26 22:03:52 -0400

    Guardando

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos' },
+    { id: 'CLP', nombre: 'Peso Chileno' },
+    { id: 'EUR', nombre: 'Euro' },
+    { id: 'GBP', nombre: 'Libreria Esterlina' },
+]
+
+
+export {
+    monedas
+}
